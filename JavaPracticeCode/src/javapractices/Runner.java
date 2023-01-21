b638b0566b9531f6c1076f8719d10d84cf2d0e01
MohanKumarR13 <svgvedha@gmail.com>
2023-01-21 17:13:49 +0530

    Initial

---
+++ JavaPracticeCode/src/javapractices/Runner.java
@@ -0,0 +1,18 @@
+package javapractices;
+
+public class Runner {
+		public static void main(String[] args) {
+			
+			Parent parent=new Parent() {
+				public void salary() {
+					System.out.println("Salary");
+				}
+			};
+			parent.education();
+			parent.work();
+			parent.salary();
+			Parent child =new Parent();
+			child.salary();
+		}
+		
+}
